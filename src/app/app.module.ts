8c302b76df8c8417f568faa7e54d2b0c5c0d193a
Tyler <tythedeveloper10@gmail.com>
2021-05-26 07:12:50 -0500

    update layout of card and spacing on different screen sizes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatRippleModule} from '@angular/material/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
@@ -30,6 +31,7 @@ import { MainContentComponent } from './main-content/main-content.component';
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
+    MatRippleModule,
     FlexLayoutModule
   ],
   providers: [],
