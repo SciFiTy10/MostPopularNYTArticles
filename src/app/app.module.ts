092b44f1f2f999aec768112be40c7df591067ab8
Tyler <tythedeveloper10@gmail.com>
2021-05-25 19:27:20 -0500

    set up material ui components and format sizes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,28 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { EmailedComponent } from './emailed/emailed.component';
 import { ArticleComponent } from './article/article.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { HeaderComponent } from './header/header.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EmailedComponent,
-    ArticleComponent
+    ArticleComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
